7ecfd1ac75c52137b3e4cfd05b85b024368fade8
SageKwun <sagekwun@gmail.com>
2022-02-15 23:01:07 +0800

    fix(debounce): clearTimeout位置&不用立即执行下timer未赋值

--- 防抖节流/debounce.js
+++ 防抖节流/debounce.js
@@ -1,24 +1,10 @@
 // fn是我们需要包装的事件回调, delay是每次推迟执行的等待时间
-function debounce(fn, delay) {
-  // 定时器
-  let timer = null;
-
-  // 将debounce处理结果当作函数返回
-  return (...args) => {
-    // 每次事件被触发时，都去清除之前的旧定时器
-    if (timer) clearTimeout(timer);
-    // 设立新定时器
-    timer = setTimeout(() => {
-      fn.apply(this, args);
-    }, delay);
-  };
-}
-
 function debounce(fn, delay, isImmediate) {
+  // 定时器
   let timer;
   let result;
+  // 将debounce处理结果当作函数返回
   return (...args) => {
-    if (timer) clearTimeout(timer);
     if (isImmediate) {
       let callNow = !timer;
       setTimeout(() => {
@@ -26,7 +12,10 @@ function debounce(fn, delay, isImmediate) {
       }, delay);
       if (callNow) result = fn.apply(this, args);
     } else {
-      setTimeout(() => {
+      // 每次事件被触发时，都去清除之前的旧定时器
+      if (timer) clearTimeout(timer);
+      // 设立新定时器
+      timer = setTimeout(() => {
         result = fn.apply(this, args);
       }, delay);
     }
